a23add628ef1908faca50c290d5edaf3a65c0a7b
smsaifurrahman <smsaifur525@gmail.com>
2024-10-24 15:27:29 +0200

    Update Api created

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -50,6 +50,27 @@ app.MapPost("games", (CreateGameDto newGame)=>
     return Results.CreatedAtRoute(GetGameEndPointName, new {id= game.Id}, game );
 });
 
+// PUT /games
+app.MapPut("games/{id}", (int id, UpdateGameDto updateGame) => {
+    var index = games.FindIndex(game => game.Id == id);
+    games[index] = new GameDto(
+        id,
+        updateGame.Name,
+        updateGame.Genre,
+        updateGame.Price,
+        updateGame.ReleaseDate
+
+    );
+    return Results.NoContent();
+});
+
+// DELETE /games/1
+app.MapDelete("games/{id}", (int id)=> 
+{
+  games.RemoveAll(game => game.Id == id);
+
+  return Results.NoContent;
+});
 
 
 app.Run();
